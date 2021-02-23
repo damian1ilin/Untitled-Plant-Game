6ec844f8c85bbbba5b499d9f1824dfc07bbd55aa
Fred Newton <ghostlopment@gmail.com>
2021-02-23 14:48:31 +0100

    stuff

--- Assets/Scripts/AgentPathfinding.cs
+++ Assets/Scripts/AgentPathfinding.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pathfinding;
 
+[RequireComponent(typeof(Seeker), typeof(AIPath), typeof(Funnel))]
 public class AgentPathfinding : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Seeker m_Seeker;
+    private AIPath m_AIPath;
+
+    [SerializeField] private GameObject m_Target;
+
+    private void Start()
     {
-        
+        m_Seeker = GetComponent<Seeker>();
+        m_AIPath = GetComponent<AIPath>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        m_AIPath.destination = m_Target.transform.position;
+    }
+
+    public void setTarget(GameObject target)
+    {
+        m_Target = target;
     }
 }
